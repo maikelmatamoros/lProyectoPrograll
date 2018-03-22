e5f6a51ffacca7f47b13c667029c0b5f8edc356c
YerLeal <yeryazmin96@gmail.com>
2018-03-22 12:17:48 -0600

    Commit de prueba 1

---
+++ Tarea2LealMatamorosSemestreI2018/src/domain/Vehicle.java
@@ -0,0 +1,12 @@
+package domain;
+
+public class Vehicle {
+    
+    private String name;
+    private int year;
+    private float mileage;
+    private boolean american;
+    private int serie;
+    
+} // fin de la clase
+
