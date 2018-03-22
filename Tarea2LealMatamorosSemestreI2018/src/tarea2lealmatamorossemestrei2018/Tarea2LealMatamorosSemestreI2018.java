8199dfdd66efac696a07b86a183d5725fa9834f1
Yerlin Leal <yeryazmin96@gmail.com>
2018-03-22 12:01:52 -0600

    NetBeansProject

---
+++ Tarea2LealMatamorosSemestreI2018/src/tarea2lealmatamorossemestrei2018/Tarea2LealMatamorosSemestreI2018.java
@@ -0,0 +1,9 @@
+package tarea2lealmatamorossemestrei2018;
+
+public class Tarea2LealMatamorosSemestreI2018 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo!!");
+    } // main
+
+} // fin de la clase
