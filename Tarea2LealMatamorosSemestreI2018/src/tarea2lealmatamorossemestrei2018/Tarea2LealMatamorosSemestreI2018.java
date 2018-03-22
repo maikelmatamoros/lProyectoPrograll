ab0ca488f2dae493c062dcbe28a83fa37106d20f
YerLeal <yeryazmin96@gmail.com>
2018-03-22 12:26:21 -0600

    Commit domain, gui & file

--- Tarea2LealMatamorosSemestreI2018/src/tarea2lealmatamorossemestrei2018/Tarea2LealMatamorosSemestreI2018.java
+++ Tarea2LealMatamorosSemestreI2018/src/tarea2lealmatamorossemestrei2018/Tarea2LealMatamorosSemestreI2018.java
@@ -1,11 +1,11 @@
 package tarea2lealmatamorossemestrei2018;
 
-import javax.swing.JOptionPane;
+import gui.Window;
 
 public class Tarea2LealMatamorosSemestreI2018 {
 
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Hola Mundo");
+        Window w = new Window();
     } // main
 
 } // fin de la clase
