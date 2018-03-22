129d3bcd17424262bc598400fa23c5c4d77b9e38
YerLeal <yeryazmin96@gmail.com>
2018-03-22 12:10:26 -0600

    Commit de prueba

--- Tarea2LealMatamorosSemestreI2018/src/tarea2lealmatamorossemestrei2018/Tarea2LealMatamorosSemestreI2018.java
+++ Tarea2LealMatamorosSemestreI2018/src/tarea2lealmatamorossemestrei2018/Tarea2LealMatamorosSemestreI2018.java
@@ -1,9 +1,11 @@
 package tarea2lealmatamorossemestrei2018;
 
+import javax.swing.JOptionPane;
+
 public class Tarea2LealMatamorosSemestreI2018 {
 
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!!");
+        JOptionPane.showMessageDialog(null, "Hola Mundo");
     } // main
 
 } // fin de la clase
