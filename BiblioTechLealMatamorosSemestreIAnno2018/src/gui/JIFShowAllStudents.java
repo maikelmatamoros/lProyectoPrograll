5d0bf9de48d541c324a10ee4f5c021d4e91dfb8d
maikel <maikel@maikel-Satellite-C45-C>
2018-03-29 20:13:54 -0600

    se creo la clase ShowStudent y se hicieron cambios en la ventana principal

--- BiblioTechLealMatamorosSemestreIAnno2018/src/gui/JIFShowAllStudents.java
+++ BiblioTechLealMatamorosSemestreIAnno2018/src/gui/JIFShowAllStudents.java
@@ -1,14 +1,98 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gui;
 
-/**
- *
- * @author maikel
- */
-public class JIFShowAllStudents {
+import business.StudentBusiness;
+import domain.Student;
+import java.awt.Color;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JInternalFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.event.InternalFrameEvent;
+import javax.swing.event.InternalFrameListener;
+import javax.swing.table.DefaultTableModel;
+
+public class JIFShowAllStudents extends JInternalFrame implements InternalFrameListener {
+
+    //Atributos
+    private StudentBusiness studentBusiness;
+    private JTable jtbTable1;
+    private DefaultTableModel dtmModelTable;
+    private JScrollPane scrollPane;
+    private ArrayList<Student> list;
+
+    //Constructor
+    public JIFShowAllStudents() {
+        super("Show Vehicles", false, true, false, false);
+        this.setLayout(null);
+        this.refresh();
+        this.setLocation(20, 40);
+        this.setSize(600, 350);
+        this.addInternalFrameListener(this);
+    }
+
+    //inicializa la tabla
+    public void initTable(ArrayList<Student> list) {
+
+        Object[][] vehicles = new Object[0][0];
+        String[] columNames1 = {"Name", "LastName", "Year", "Career", "ID"};
+        this.dtmModelTable = new DefaultTableModel(vehicles, columNames1);
+        for (int i = 0; i < list.size(); i++) {
+
+            this.dtmModelTable.addRow(new Object[]{list.get(i).getName(),
+                list.get(i).getLastName(), list.get(i).getYear(), list.get(i).getCarrera(), list.get(i).getId()});
+
+        }//for
+        this.jtbTable1 = new JTable(this.dtmModelTable);
+        this.scrollPane = new JScrollPane(this.jtbTable1);
+        scrollPane.setBounds(5, 5, 300, 285);
+        this.jtbTable1.setSelectionBackground(Color.GREEN);
+        this.add(scrollPane);
+    } // inicializa el modelo de la tabla cargando los valores del archivo
+
+    //refresca los valores de la tabla
+    public void refresh() {
+        try {
+            this.studentBusiness = new StudentBusiness();
+            this.list = this.studentBusiness.getAllStudents();
+
+        } catch (IOException ex) {
+            Logger.getLogger(JIFShowAllStudents.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        initTable(this.list);
+
+    } // refresh
+
+    //Evento internalFrame para que cuando se cierre habilite el JMitem con el que se abre la ventana
+    @Override
+    public void internalFrameOpened(InternalFrameEvent e) {
+    }
+
+    @Override
+    public void internalFrameClosing(InternalFrameEvent e) {
+    }
+
+    @Override
+    public void internalFrameClosed(InternalFrameEvent e) {
     
-}
+    } // internalFrameClosed
+
+    @Override
+    public void internalFrameIconified(InternalFrameEvent e) {
+    }
+
+    @Override
+    public void internalFrameDeiconified(InternalFrameEvent e) {
+    }
+
+    @Override
+    public void internalFrameActivated(InternalFrameEvent e) {
+    }
+
+    @Override
+    public void internalFrameDeactivated(InternalFrameEvent e) {
+    }
+
+} // fin de la clase
--- BiblioTechLealMatamorosSemestreIAnno2018/src/gui/MainWindows.java
+++ BiblioTechLealMatamorosSemestreIAnno2018/src/gui/MainWindows.java
@@ -8,6 +8,7 @@ package gui;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
@@ -18,11 +19,9 @@ import javax.swing.JMenuItem;
  */
 public class MainWindows extends JFrame implements ActionListener{
     private JMenuBar jMenuBar;
-    private JMenuItem jmiNewStudent,jmiDeleteStudent,jmiUpdateStudent;
+    private JMenuItem jmiNewStudent,jmiDeleteStudent,jmiUpdateStudent,jmiShowStudents;
     private JMenu jmStudent;
-    private JIFNewStudent jIFNewStudent;
-    private JIFStudentDelete jIFStudentDelete;
-    private JIFStudentUpdate jIFStudentUpdate;
+    private JInternalFrame jIFNewStudent,jiFShowAllStudents,jIFStudentUpdate,jIFStudentDelete;
     public MainWindows(){
         super("BiblioTech");
         this.setResizable(false);
@@ -42,16 +41,19 @@ public class MainWindows extends JFrame implements ActionListener{
         this.jmiNewStudent=new JMenuItem("New Student");
         this.jmiDeleteStudent=new JMenuItem("Delete Student");
         this.jmiUpdateStudent=new JMenuItem("Update Student");
+        this.jmiShowStudents=new JMenuItem("Show Students");
         
         this.jmiNewStudent.addActionListener(this);
         this.jmiDeleteStudent.addActionListener(this);
         this.jmiUpdateStudent.addActionListener(this);
+        this.jmiShowStudents.addActionListener(this);
         
         this.jMenuBar.setSize(700, 40);
         
         this.jmStudent.add(this.jmiNewStudent);
         this.jmStudent.add(this.jmiDeleteStudent);
         this.jmStudent.add(this.jmiUpdateStudent);
+        this.jmStudent.add(this.jmiShowStudents);
         this.jMenuBar.add(this.jmStudent);
         this.add(this.jMenuBar);
     }
@@ -70,6 +72,10 @@ public class MainWindows extends JFrame implements ActionListener{
             this.jIFStudentUpdate=new JIFStudentUpdate();
             this.add(this.jIFStudentUpdate);
             this.jIFStudentUpdate.setVisible(true);
+        }else if(e.getSource()==this.jmiShowStudents){
+            this.jiFShowAllStudents=new JIFShowAllStudents();
+            this.add(this.jiFShowAllStudents);
+            this.jiFShowAllStudents.setVisible(true);
         }
     }
 }
