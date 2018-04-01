280a09cbac3bdab99f6ca2769889287da5e8c0c2
YerLeal <yeryazmin96@gmail.com>
2018-03-31 18:51:29 -0600

    Commit prueba

--- BiblioTechLealMatamorosSemestreIAnno2018/src/bibliotechlealmatamorossemestreianno2018/BiblioTechLealMatamorosSemestreIAnno2018.java
+++ BiblioTechLealMatamorosSemestreIAnno2018/src/bibliotechlealmatamorossemestreianno2018/BiblioTechLealMatamorosSemestreIAnno2018.java
@@ -6,6 +6,7 @@ public class BiblioTechLealMatamorosSemestreIAnno2018 {
 
     public static void main(String[] args) {
          MainWindows mainWindows=new MainWindows();
+         String name = "Yer";
     } // main
     
 } // fin de la clase
